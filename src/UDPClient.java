bd2c9b376c318f31eda1e7dab66c37ec90a933b9
Will Spencer <willspencer171@gmail.com>
2024-05-08 18:03:43 +0100

    Modified and Completed UDP Client-Server

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -3,6 +3,10 @@ import java.net.*;
 class UDPClient {
     public static void main(String[] args) throws Exception
     {
+        /*When looking at UDP, it's important to remember that there is no connection between
+        * the two sockets, so we use a DatagramSocket object. When I create a TCP Client-Server, I'll
+        * try to remember to highlight where the TCP Socket object is used */
+
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         DatagramSocket clientSocket = new DatagramSocket();
         InetAddress IPAddress = InetAddress.getByName("localhost");
@@ -15,7 +19,7 @@ class UDPClient {
         DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length);
         clientSocket.receive(receivePacket);
         String modifiedSentence = new String(receivePacket.getData());
-        System.out.println("FROM SERVER: " + modifiedSentence);
+        System.out.println("FROM SERVER at " + receivePacket.getAddress().getHostAddress() + ":" + receivePacket.getPort() + " - " + modifiedSentence.trim());
         clientSocket.close();
     }
 }
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,17 +1,21 @@
 import java.net.*;
+import java.nio.charset.StandardCharsets;
+
 class UDPServer {
     public static void main(String[] args) throws Exception
     {
         DatagramSocket serverSocket = new DatagramSocket(9876);
         byte[] receiveData = new byte[1024];
         byte[] sendData;
+        System.out.println("Server Running!");
         while(true)
         {
             DatagramPacket receivePacket = new DatagramPacket(receiveData,
                     receiveData.length);
             serverSocket.receive(receivePacket);
-            String sentence = new String(receivePacket.getData());
+            String sentence = new String(receivePacket.getData()).trim();
             InetAddress IPAddress = receivePacket.getAddress();
+            System.out.println("FROM CLIENT at " + IPAddress.getHostName() + ":" + receivePacket.getPort() + " - " + sentence);
 
             int port = receivePacket.getPort();
             String capitalizedSentence = sentence.toUpperCase();
