7f456cb1c38f58f536b7065e19bb2f783b5150ad
Will Spencer <willspencer171@gmail.com>
2024-05-12 15:24:15 +0100

    Commented code for legibility and compatibilty with README

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+
 class UDPClient {
     public static void main(String[] args) throws Exception
     {
@@ -7,16 +8,23 @@ class UDPClient {
         * the two sockets, so we use a DatagramSocket object. When I create a TCP Client-Server, I'll
         * try to remember to highlight where the TCP Socket object is used */
 
+        // Terminal input
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        // Create socket at random port
         DatagramSocket clientSocket = new DatagramSocket();
+        // Get IP address from name "localhost"
         InetAddress IPAddress = InetAddress.getByName("localhost");
         byte[] sendData;
+        // Buffer used to receive data
         byte[] receiveData = new byte[1024];
         String sentence = inFromUser.readLine();
         sendData = sentence.getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,IPAddress, 9876);
+        // Generate outgoing packet
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
         clientSocket.send(sendPacket);
+
         DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length);
+        // block until packet received
         clientSocket.receive(receivePacket);
         String modifiedSentence = new String(receivePacket.getData());
         System.out.println("FROM SERVER at " + receivePacket.getAddress().getHostAddress() + ":" + receivePacket.getPort() + " - " + modifiedSentence.trim());
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,27 +1,48 @@
+import java.io.IOException;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
 
 class UDPServer {
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
+        // Create socket at port 9876
         DatagramSocket serverSocket = new DatagramSocket(9876);
+        // Buffer for receiving data (size 1024
         byte[] receiveData = new byte[1024];
+        // Buffer for sending data
         byte[] sendData;
         System.out.println("Server Running!");
         while(true)
         {
+            // Packet containing the buffer
             DatagramPacket receivePacket = new DatagramPacket(receiveData,
                     receiveData.length);
+            // Blocks execution until a packet is received on port
             serverSocket.receive(receivePacket);
+            // Trim data down from 1024 to the length of the string
             String sentence = new String(receivePacket.getData()).trim();
+
+            // Get return address
             InetAddress IPAddress = receivePacket.getAddress();
+            // Get return port
+            int port = receivePacket.getPort();
+
+            // Close server if user input is '.'
+            if (sentence.equals(".")) {
+                serverSocket.send(new DatagramPacket("Closing server".getBytes(), "Closing server".length(), IPAddress, port));
+                serverSocket.close();
+                break;
+            }
+
             System.out.println("FROM CLIENT at " + IPAddress.getHostName() + ":" + receivePacket.getPort() + " - " + sentence);
 
-            int port = receivePacket.getPort();
+            // Uppercase input
             String capitalizedSentence = sentence.toUpperCase();
+            // Convert to bytes
             sendData = capitalizedSentence.getBytes();
+            // Generate packet
             DatagramPacket sendPacket = new DatagramPacket(sendData,
                     sendData.length, IPAddress, port);
+            // Send packet
             serverSocket.send(sendPacket);
         }
     }
